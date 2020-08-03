bbbfa83a374cfb0ceac0d1a57e7dbdc218d13c50
Juyeong-Kim <64299755+Juyeong-Kim@users.noreply.github.com>
2020-08-03 16:02:23 +0900

    Updates

---
+++ precache-manifest.aa2816a023766e4f8bee864db74c944f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9c0daff5cd57e07a3e79b5bfbd7f1a",
+    "url": "/movie_app_jubbang/index.html"
+  },
+  {
+    "revision": "aa6f9afadd29c648e381",
+    "url": "/movie_app_jubbang/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "01814a1cd22774391acd",
+    "url": "/movie_app_jubbang/static/js/2.f99f4147.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_jubbang/static/js/2.f99f4147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa6f9afadd29c648e381",
+    "url": "/movie_app_jubbang/static/js/main.f043c25c.chunk.js"
+  },
+  {
+    "revision": "ac5fbcea34ce0d5ffb9a",
+    "url": "/movie_app_jubbang/static/js/runtime-main.af393733.js"
+  }
+]);
